fd7c93c99dab3de26f4fe80f9ebbab855c783392
tylerjacobmueller08@gmail.com <tylerjacobmueller08@gmail.com>
2026-01-11 17:03:52 -0700

    Updated PathPlannerLib to 2026

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026RapidAcceleration";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 4;
-    public static final String GIT_SHA = "e926f6f27d4d0d3f47ca5e3925a175ab329efcb5";
-    public static final String GIT_DATE = "2026-01-10 16:18:06 MST";
+    public static final int GIT_REVISION = 5;
+    public static final String GIT_SHA = "9b21e539b713f345460e3d38d1cf2936a23c0ff3";
+    public static final String GIT_DATE = "2026-01-11 11:36:51 MST";
     public static final String GIT_BRANCH = "swerve";
-    public static final String BUILD_DATE = "2026-01-11 11:30:28 MST";
-    public static final long BUILD_UNIX_TIME = 1768156228665L;
+    public static final String BUILD_DATE = "2026-01-11 17:03:24 MST";
+    public static final long BUILD_UNIX_TIME = 1768176204527L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
