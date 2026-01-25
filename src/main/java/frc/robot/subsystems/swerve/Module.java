3498d9038408581148fbf641462146d1175c39ff
tylerjacobmueller08@gmail.com <tylerjacobmueller08@gmail.com>
2026-01-25 13:33:06 -0700

    Resolved swerve deprecation and parameterization warnings

--- src/main/java/frc/robot/subsystems/swerve/Module.java
+++ src/main/java/frc/robot/subsystems/swerve/Module.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.swerve;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -14,14 +16,17 @@ public class Module {
     private final ModuleIO io;
     private final ModuleInputsAutoLogged inputs = new ModuleInputsAutoLogged();
     private final int index;
-    private final SwerveModuleConstants constants;
+    private final SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> constants;
 
     private final Alert driveDisconnectedAlert;
     private final Alert turnDisconnectedAlert;
     private final Alert turnEncoderDisconnectedAlert;
     private SwerveModulePosition[] odometryPositions = new SwerveModulePosition[] {};
 
-    public Module(ModuleIO io, int index, SwerveModuleConstants constants) {
+    public Module(
+            ModuleIO io,
+            int index,
+            SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> constants) {
         this.io = io;
         this.index = index;
         this.constants = constants;
