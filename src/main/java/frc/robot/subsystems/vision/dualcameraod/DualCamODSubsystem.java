9604fbbad5f602f6107c6c1274531704dee370cf
Michael Schnee <maschnee11@icloud.com>
2026-02-02 20:18:45 -0700

    (Slowly) added wanted functions to Dual Camera Object Detection

--- src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODSubsystem.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems.vision.dualcameraod;
 
+import edu.wpi.first.math.geometry.*;
+import frc.robot.subsystems.vision.objectdetection.ObjectDetectionIO;
+
+//A lot of the wanted code/protocols are already in the object detection package
+
 public class DualCamODSubsystem {
+	
 	//At this point, I have no idea whether this will work or not
+	
+	private final ObjectDetectionIO ioA;
+	private final ObjectDetectionIO ioB;
+
+	public DualCamODSubsystem(ObjectDetectionIO ioA, ObjectDetectionIO ioB) {
+		this.ioA = ioA;
+		this.ioB = ioB;
+	}
+
+	/*What we know we want
+		1. A method I already know that turns two targets into one target w/ info stored as a Pose3D...
+		We'll write it as a Pose2D for now
+	*/
+
+	public /*possibly change that */ Pose2d positionFromDualYaw (ObjectDetectionIO.TargetObservation a, ObjectDetectionIO.TargetObservation b){
+		double distanceBetweenCams = KSyncableCameras[0].cameraToFrontCenter.getX() - KSyncableCameras[1].cameraToFrontCenter.getX();
+	}
+
+
+	/*What I'm scared about
+		1. Will the robot have performance issues due to large amounts of complex calcs?
+	*/
 }
