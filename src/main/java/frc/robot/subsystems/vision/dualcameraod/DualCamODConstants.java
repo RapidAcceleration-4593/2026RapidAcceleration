c05da8034d9bec44a4ca66eeb0fce9aa1d1737f7
Michael Schnee <maschnee11@icloud.com>
2026-02-03 20:09:32 -0700

    got positionFromDualYaw working (in ideal scenarios only though)!

--- src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODConstants.java
+++ src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODConstants.java
@@ -6,8 +6,8 @@ import edu.wpi.first.math.geometry.Transform3d;
 
 public final class DualCamODConstants {
 	//Declare these terms when we know what they are
-	public record SyncableCameraConfig(String name, Transform3d cameraToFrontCenter, double yawEdge, double pitchEdge) {}
+	public record SyncableCameraConfig(String name, Transform3d robotToCamera, double yawEdge, double pitchEdge) {}
 	//yawedge and pitchedge refer to parts that won't show in other camera views
-	public static final SyncableCameraConfig[] KSyncableCameras = {};
+	public static final SyncableCameraConfig[] kSyncableCameras = {};
 	//again, add cameras once we know what we're doing, which I currently really don't
 }
--- src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.vision.dualcameraod;
 
 import edu.wpi.first.math.geometry.*;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
 import frc.robot.subsystems.vision.objectdetection.ObjectDetectionIO;
 
 //A lot of the wanted code/protocols are already in the object detection package
@@ -11,24 +12,35 @@ public class DualCamODSubsystem {
 	
 	private final ObjectDetectionIO ioA;
 	private final ObjectDetectionIO ioB;
+	final SwerveSubsystem swerve;
 
-	public DualCamODSubsystem(ObjectDetectionIO ioA, ObjectDetectionIO ioB) {
+	public DualCamODSubsystem(ObjectDetectionIO ioA, ObjectDetectionIO ioB, SwerveSubsystem swerve) {
 		this.ioA = ioA;
 		this.ioB = ioB;
+		this.swerve = swerve;
 	}
 
-	/*What we know we want
-		1. A method I already know that turns two targets into one target w/ info stored as a Pose3D...
-		We'll write it as a Pose2D for now
+	/**This function uses the yaw of two cameras to determine the position of a target.
+	 * Right now, it returns a Pose2D, though we should eventually edit it to return a Pose3D.
+	 * TODO: right now, it requires both cameras to be exactly parallel, change that, I'm too lazy tonight)
 	*/
-
-	public /*possibly change that */ Pose2d positionFromDualYaw (ObjectDetectionIO.TargetObservation a, ObjectDetectionIO.TargetObservation b){
-		double distanceBetweenCams = KSyncableCameras[0].cameraToFrontCenter.getX() - KSyncableCameras[1].cameraToFrontCenter.getX();
+	public Pose2d positionFromDualYaw (ObjectDetectionIO.TargetObservation a, ObjectDetectionIO.TargetObservation b){
+		double distanceBetweenCams = DualCamODConstants.kSyncableCameras[0].robotToCamera().getX() - DualCamODConstants.kSyncableCameras[1].robotToCamera().getX();
 		double unscaledDistance = (Math.cos(b.yaw().getRadians()) * Math.sin(a.yaw().getRadians())) / (Math.sin(b.yaw().getRadians())) + Math.cos(a.yaw().getRadians());
-		//equation seems wrong, check this later
 		double scaleFactor = distanceBetweenCams/unscaledDistance;
+		//transform our distances from robot center based on robot pose
+		Pose2d posFromRobot = new Pose2d(
+			-(distanceBetweenCams/2 - Math.cos(a.yaw().getRadians()) * scaleFactor),
+			Math.sin(a.yaw().getRadians()) * scaleFactor + DualCamODConstants.kSyncableCameras[0].robotToCamera().getY(),
+			Rotation2d.fromDegrees(0)
+			);
+		//now that it's robot to camera, I need to add that y
+		Transform2d absolutePose = new Transform2d(new Pose2d(), this.swerve.getPose());
+		return posFromRobot.transformBy(absolutePose);
 	}
 
+	//Ideally, if I understand gaussian distribution well enough, I could use that to figure out whether our measures are behaving under Kalman principles
+	//... or if they're moving
 
 	/*What I'm scared about
 		1. Will the robot have performance issues due to large amounts of complex calcs?
