fc5dc7bd699b25e59fbb34bb908234629db6a3fb
Michael Schnee <maschnee11@icloud.com>
2026-02-07 15:53:34 -0700

    Properly added other changes that I hadn't saved

--- src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODConstants.java
+++ src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODConstants.java
@@ -1,12 +1,9 @@
 package frc.robot.subsystems.vision.dualcameraod;
 
-import edu.wpi.first.math.geometry.Transform3d;
-
 //I suspect that I'm writing straight garbage...
 
 public final class DualCamODConstants {
 	//Declare these terms when we know what they are
-	public record SyncableCameraConfig(String name, Transform3d robotToCamera, double yawEdge, double pitchEdge) {}
 	//yawedge and pitchedge refer to parts that won't show in other camera views
 	public static final SyncableCameraConfig[] kSyncableCameras = {};
 	//again, add cameras once we know what we're doing, which I currently really don't
