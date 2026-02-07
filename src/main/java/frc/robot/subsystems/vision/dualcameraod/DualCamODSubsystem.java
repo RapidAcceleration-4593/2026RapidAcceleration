4cf0dc2016cec5a147aaef40167e67211291e6cf
Michael Schnee <maschnee11@icloud.com>
2026-02-07 13:17:56 -0700

    Got iterateInputs finished, although more work is needed for more complex situations

--- src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/dualcameraod/DualCamODSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.vision.dualcameraod;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import edu.wpi.first.math.geometry.*;
@@ -18,6 +19,8 @@ public class DualCamODSubsystem extends SubsystemBase{
 	
 	private final ObjectDetectionIO ioA, ioB;
 	private final ObjectDetectionInputsAutoLogged inputsA, inputsB;
+	List<Pose2d> TargetPoses;
+		//eventually create time focused data structure, or, at least a better structure than just this.
 	final SwerveSubsystem swerve;
 
 	/**This code assumes a few other things that may not be true, namely that
@@ -38,26 +41,30 @@ public class DualCamODSubsystem extends SubsystemBase{
 		ioB.updateInputs(inputsB);
 	}
 
+	/**Adds poses from current inputs to target poses list, TODO: set it to split things up by timestamp and delete lists that aren't needed*/
 	public void iterateInputs(){
-		Stream<TargetObservation> bInputArray = Arrays.stream(inputsB.latestTargets)
+		
+		List<TargetObservation> bInputList = Arrays.stream(inputsB.latestTargets)
 			.filter(b -> b.yaw().getDegrees() < DualCamODConstants.kSyncableCameras[0].yawEdge())
 			.filter(b -> b.pitch().getDegrees() > DualCamODConstants.kSyncableCameras[0].pitchEdge())
-			.sorted((a, b) -> Double.compare(a.yaw().getDegrees(), b.yaw().getDegrees()));
+			.sorted((a, b) -> Double.compare(a.yaw().getDegrees(), b.yaw().getDegrees()))
+			.toList();
 			/*I doubt that this is the best way to do it with a significant enough ambiguity.
 			Ideally, if ambiguity exceeds a specific level, the pitch should be what we sort it by.
 			Right now, however, pitch plays no part in our results, so yaw is probably good enough.*/
-		Stream<TargetObservation> aInputArray = Arrays.stream(inputsA.latestTargets)
+		List<TargetObservation> aInputList = Arrays.stream(inputsA.latestTargets)
 			.filter(a -> a.yaw().getDegrees() > DualCamODConstants.kSyncableCameras[0].yawEdge())
 			.filter(a -> a.pitch().getDegrees() > DualCamODConstants.kSyncableCameras[0].yawEdge())
-			.sorted((a, b) -> Double.compare(a.yaw().getDegrees(), b.yaw().getDegrees()));
-			//filter for current timestamp (probably eventually delete this line if we can change results to work MY WAY)
-			//filter out yaws greater than yawedge, or less for the alternative
-			//sort by pitch, then yaw
+			.sorted((a, b) -> Double.compare(a.yaw().getDegrees(), b.yaw().getDegrees()))
+			.toList();
+		
+		for(int i = 0; i<aInputList.size(); i++){
+			TargetPoses.add(positionFromDualYaw(aInputList.get(i), bInputList.get(i)));
+		}
 			//map with others, run positionfromyaw - probably use a new array & a loop at this point, unless streams can make this easier
 		
 		//We don't want a single timestamp, but we need a way to split the poses we get into separate time stamps
 		//Ideally, we'd want to lower the timestamped data we store based on how long ago it was - the farther away, the less we need to keep
-		//otherwise, we filter each by what isn't on the edge of the cameras, sort by pitch, then yaw, and, from there, it's pretty easy to match targets and run positionfromyaw :)
 	}
 
 	/**This function uses the yaw of two cameras to determine the position of a target.
