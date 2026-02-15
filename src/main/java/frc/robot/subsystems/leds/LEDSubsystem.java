a66dd10156de48c50fd96c854fc844c5392e816f
Cuber98 <cubemusic53@gmail.com>
2026-02-14 18:00:20 -0700

    Created LEDSubsystem

---
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.leds;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase {
+	
+	public LEDSubsystem() {
+		
+	}
+}
