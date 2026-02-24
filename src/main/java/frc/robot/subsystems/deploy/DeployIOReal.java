347e313426869602a94ab4149c0242e6a5e3805b
Michael Schnee <maschnee11@icloud.com>
2026-02-23 20:16:03 -0700

    Removed no-longer-needed import statements

--- src/main/java/frc/robot/subsystems/deploy/DeployIOReal.java
+++ src/main/java/frc/robot/subsystems/deploy/DeployIOReal.java
@@ -13,7 +13,6 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.AlternateEncoderConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig;
-import com.revrobotics.spark.config.SoftLimitConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
--- src/main/java/frc/robot/subsystems/deploy/DeploySubsystem.java
+++ src/main/java/frc/robot/subsystems/deploy/DeploySubsystem.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
