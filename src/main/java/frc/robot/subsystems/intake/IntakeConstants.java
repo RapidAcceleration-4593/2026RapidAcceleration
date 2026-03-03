7f8d6db1dc6fcbfac027c58f39251e2521ef6573
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-03-02 20:48:47 -0700

    Updated intake speed and gear ratio; updated autonomous paths

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -13,8 +13,8 @@ public final class IntakeConstants {
     public static final double kMotorVelocityIntakeThreshold = 0.1;
     public static final int kMaxCapacity = 30;
 
-    public static final Voltage kIntakeVolts = Volts.of(10);
-    public static final double kIntakeGearing = 5.0 * (16.0 / 16.0);
+    public static final Voltage kIntakeVolts = Volts.of(6);
+    public static final double kIntakeGearing = 3.0 * (16.0 / 16.0);
 
     public static final MomentOfInertia kIntakeMOI = KilogramSquareMeters.of(0.0004);
 }
