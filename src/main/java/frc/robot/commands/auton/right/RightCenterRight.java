fe06d3c453ecf1ff7db43427110be1cf7758f8f1
Michael Schnee <maschnee11@icloud.com>
2026-03-04 20:22:43 -0700

    Created RightCenterRight path

---
+++ src/main/java/frc/robot/commands/auton/right/RightCenterRight.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.auton.right;
+
+import static edu.wpi.first.units.Units.Seconds;
+
+import java.util.List;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.auton.AutonCommand;
+import frc.robot.commands.auton.AutonUtil;
+
+public class RightCenterRight extends AutonCommand{
+	public RightCenterRight(AutonUtil util){
+		super(util, List.of("RightCenter-1","RightCenter-2","RightClimbRight"));
+
+		addCommands(
+			Commands.race(	
+				Commands.waitSeconds(10).andThen(NamedCommands.getCommand("IntakeCommand")),
+				AutoBuilder.followPath(paths.get(0))
+			),
+			AutoBuilder.followPath(paths.get(1)),
+			NamedCommands.getCommand("ShootShakeCommands").withTimeout(Seconds.of(10.0)),
+			AutoBuilder.followPath(paths.get(2)),
+			NamedCommands.getCommand("ClimbCommand").withTimeout(Seconds.of(5.0))
+			//ClimbCommand does nothing now, timeout soon will be useless (hopefully)
+		);
+	}
+}
