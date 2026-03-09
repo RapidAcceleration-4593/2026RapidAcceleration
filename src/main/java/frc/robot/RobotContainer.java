5dbfab099d0c248ddbd4075b74559151a56d1665
TheCrackerSnacker <mvoncrackersnackin@gmail.com>
2026-03-09 16:55:49 -0600

    Fixed turret manual control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -126,7 +126,7 @@ public class RobotContainer {
         operatorController.povUp().whileTrue(climber.setVoltageCommand(Volts.of(12.0)));
         operatorController.povDown().whileTrue(climber.setVoltageCommand(Volts.of(-12.0)));
 
-        operatorController.start().onTrue(turret.runOnce(() -> turret.setDefaultCommand(Commands.none())));
+        operatorController.start().onTrue(turret.runOnce(() -> turret.setDefaultCommand(turret.idle())));
 
         operatorController
                 .back()
