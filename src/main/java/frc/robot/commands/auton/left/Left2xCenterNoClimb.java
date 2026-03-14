e35d540098a2f3acb13d69892ae709f36b54bd02
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-03-14 10:47:03 -0500

    Update at 'Sat Mar 14 10:47:02 CDT 2026'

--- src/main/java/frc/robot/commands/auton/left/Left2xCenterNoClimb.java
+++ src/main/java/frc/robot/commands/auton/left/Left2xCenterNoClimb.java
@@ -14,7 +14,7 @@ public class Left2xCenterNoClimb extends AutonCommand {
         addCommands(
                 NamedCommands.getCommand("IntakeCommand").withDeadline(AutoBuilder.followPath(paths.get(0))),
                 AutoBuilder.followPath(paths.get(1)),
-                NamedCommands.getCommand("ShootShakeCommand").withTimeout(5.5),
+                NamedCommands.getCommand("ShootShakeCommand").withTimeout(8.0),
                 NamedCommands.getCommand("IntakeCommand").withDeadline(AutoBuilder.followPath(paths.get(2))),
                 AutoBuilder.followPath(paths.get(3)),
                 NamedCommands.getCommand("ShootShakeCommand").withTimeout(7.5));
