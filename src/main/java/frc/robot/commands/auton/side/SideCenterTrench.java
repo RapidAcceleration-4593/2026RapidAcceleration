4fb327aeffe3825b96e66195b3997dae1955491b
tylerjacobmueller08@gmail.com <tylerjacobmueller08@gmail.com>
2026-04-06 00:42:12 -0600

    Updated autonomous paths to include loops instead of separate paths

--- src/main/java/frc/robot/commands/auton/side/SideCenterTrench.java
+++ src/main/java/frc/robot/commands/auton/side/SideCenterTrench.java
@@ -9,11 +9,10 @@ import java.util.List;
 public class SideCenterTrench extends AutonCommand {
 
     public SideCenterTrench(AutonUtil util, boolean isFlipped) {
-        super(util, isFlipped, List.of("SideCenterTrench-1", "SideCenterTrench-2"));
+        super(util, isFlipped, List.of("SideCenterTrench-1"));
 
         addCommands(
                 NamedCommands.getCommand("IntakeCommand").withDeadline(AutoBuilder.followPath(paths.get(0))),
-                NamedCommands.getCommand("ShootCommand").withTimeout(5.0),
-                NamedCommands.getCommand("IntakeCommand").withDeadline(AutoBuilder.followPath(paths.get(1))));
+                NamedCommands.getCommand("ShootCommand").withTimeout(15.0));
     }
 }
