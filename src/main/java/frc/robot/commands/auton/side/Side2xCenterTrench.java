b516c2c5c7b15884c205228233ef8af3bd8da524
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-04-08 16:51:55 -0600

    Created Event Markers to trigger shoot command before finishing the path

--- src/main/java/frc/robot/commands/auton/side/Side2xCenterTrench.java
+++ src/main/java/frc/robot/commands/auton/side/Side2xCenterTrench.java
@@ -2,6 +2,7 @@ package frc.robot.commands.auton.side;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.events.EventTrigger;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.auton.AutonCommand;
 import frc.robot.commands.auton.AutonUtil;
@@ -11,15 +12,20 @@ public class Side2xCenterTrench extends AutonCommand {
 
     public Side2xCenterTrench(AutonUtil util, boolean isFlipped) {
         super(util, isFlipped, List.of("SideCenterTrench-1", "SideCenterTrench-2", "SideCenterTrench-3"));
+        EventTrigger shootTrigger = new EventTrigger("Shoot");
 
         addCommands(
-                Commands.race(
-                        AutoBuilder.followPath(paths.get(0)),
-                        NamedCommands.getCommand("ExtendDeployCommand")
-                                .andThen(NamedCommands.getCommand("IntakeCommand"))),
-                NamedCommands.getCommand("ShootCommand").withTimeout(5.0),
+                Commands.parallel(
+                        Commands.race(
+                                AutoBuilder.followPath(paths.get(0)),
+                                NamedCommands.getCommand("ExtendDeployCommand")
+                                        .andThen(NamedCommands.getCommand("IntakeCommand"))),
+                        Commands.waitUntil(shootTrigger)
+                                .andThen(
+                                        NamedCommands.getCommand("ShootCommand").withTimeout(5.0))),
                 NamedCommands.getCommand("IntakeCommand").withDeadline(AutoBuilder.followPath(paths.get(1))),
-                AutoBuilder.followPath(paths.get(2)),
-                NamedCommands.getCommand("ShootCommand"));
+                Commands.parallel(
+                        AutoBuilder.followPath(paths.get(2)),
+                        Commands.waitUntil(shootTrigger).andThen(NamedCommands.getCommand("ShootCommand"))));
     }
 }
