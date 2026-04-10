6f98609698898a50f10e87c06ddd5d2efba1c50d
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-04-10 16:26:19 -0600

    Update at 'Fri Apr 10 16:26:19 MDT 2026'

--- src/main/java/frc/robot/commands/auton/side/Side2xCenterTrench.java
+++ src/main/java/frc/robot/commands/auton/side/Side2xCenterTrench.java
@@ -22,17 +22,11 @@ public class Side2xCenterTrench extends AutonCommand {
                                         .andThen(NamedCommands.getCommand("RetractDeployCommand"))
                                         .andThen(NamedCommands.getCommand("IntakeCommand"))),
                         Commands.waitUntil(shootTrigger)
-                                .andThen(Commands.parallel(
-									Commands.waitSeconds(2.5).andThen(NamedCommands.getCommand("ShakeDeployCommand")),
-									NamedCommands.getCommand("ShootCommand")
-								).withTimeout(5.0)
-                                        )),
+                                .andThen(
+                                        NamedCommands.getCommand("ShootCommand").withTimeout(5.0))),
                 NamedCommands.getCommand("IntakeCommand").withDeadline(AutoBuilder.followPath(paths.get(1))),
                 Commands.parallel(
                         AutoBuilder.followPath(paths.get(2)),
-                        Commands.waitUntil(shootTrigger).andThen(Commands.parallel(
-									Commands.waitSeconds(2.5).andThen(NamedCommands.getCommand("ShakeDeployCommand")),
-									NamedCommands.getCommand("ShootCommand")
-								).withTimeout(5.0))));
+                        Commands.waitUntil(shootTrigger).andThen(NamedCommands.getCommand("ShootCommand"))));
     }
 }
