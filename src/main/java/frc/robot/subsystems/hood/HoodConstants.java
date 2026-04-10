31e3b70ec5c0c91f77a092c11df5f5e97744b05c
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-04-10 08:22:57 -0600

    Update at 'Fri Apr 10 08:22:57 MDT 2026'

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -12,7 +12,7 @@ public final class HoodConstants {
 
     public static final boolean kInvertMotor = true;
     public static final boolean kInvertEncoder = true;
-    public static final boolean kInvertLS = false;
+    public static final boolean kInvertLS = true;
 
     public static final double kP = 0.24;
     public static final double kI = 0.0;
