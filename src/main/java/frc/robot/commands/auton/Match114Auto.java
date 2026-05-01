0bd81a8b2c8d87869528967f01186bf1b03591d7
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-05-01 16:00:21 -0500

    Update at 'Fri May 01 16:00:21 CDT 2026'

---
+++ src/main/java/frc/robot/commands/auton/Match114Auto.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.auton;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj2.command.Commands;
+import java.util.List;
+
+public class Match114Auto extends AutonCommand {
+
+    public Match114Auto(AutonUtil util, boolean isFlipped) {
+        super(util, isFlipped, List.of("Match114-1", "Match114-2"));
+
+        addCommands(
+			Commands.deadline(
+				Commands.waitSeconds(6.0),
+				NamedCommands.getCommand("ShootCommand"),
+				Commands.sequence(
+					NamedCommands.getCommand("ExtendDeployCommand"),
+					NamedCommands.getCommand("RetractDeployCommand")
+				)
+			),
+			Commands.race(
+				AutoBuilder.followPath(paths.get(0)),
+				NamedCommands.getCommand("IntakeCommand")
+			),
+			AutoBuilder.followPath(paths.get(1)),
+			Commands.parallel(
+				NamedCommands.getCommand("ShootCommand"),
+				NamedCommands.getCommand("ShakeDeployCommand")
+			));
+    }
+}
